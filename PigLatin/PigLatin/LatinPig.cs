21d7236c5e594a78711525f1f66937e84184bfb6
Ramona Mihai <ramona.mihai87@yahoo.com>
2018-12-16 13:02:35 +0200

    Pig latin

---
+++ PigLatin/PigLatin/LatinPig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PigLatin
+{
+    public static class LatinPig
+    {
+        static char[] vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+        public static string TransformWord(string input)
+        {
+            if (input.Length > 0)
+            {
+                if (Array.IndexOf(vowels, char.ToLower(input[0])) >= 0)
+                {
+                    return input + "ay";
+                }
+                else
+                {
+                    return input.Substring(1) + input.Substring(0, 1).ToLower() + "ay";
+                }                             
+            }
+            return "";
+        }
+    }
+}
---
+++ PigLatin/PigLatin/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PigLatin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Test will be transformed in: \"{LatinPig.TransformWord("Test")}\".");
+            Console.WriteLine($"Examen will be transformed in: \"{LatinPig.TransformWord("Examen")}\".");
+            Console.WriteLine($"Empty string will be transformed in: \"{LatinPig.TransformWord(string.Empty)}\".");
+
+        }
+    }
+}
